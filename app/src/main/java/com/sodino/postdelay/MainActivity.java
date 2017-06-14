e0ac349f6a4a0b833d939bfceeaaf1d5ea2c976a
sodino <sodino@qq.com>
2017-06-14 14:12:11 +0800

    init project

---
+++ app/src/main/java/com/sodino/postdelay/MainActivity.java
@@ -0,0 +1,67 @@
+package com.sodino.postdelay;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Handler.Callback {
+    private static final int DO_BG_TASK = 1;
+    private static final int DO_UI_TASK = 2;
+
+    private static final int DURATION = 1000;
+
+    HandlerThread bThread = new  HandlerThread("backendHandler_thread", Process.THREAD_PRIORITY_BACKGROUND);
+    Handler backHandler;
+    Handler uiHandler = new Handler(this);
+    int bgCount = 0;
+    int uiCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStart).setOnClickListener(this);
+        findViewById(R.id.btnStop).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnStart:{
+                if (backHandler == null) {
+                    bThread.start();
+                    backHandler = new Handler(bThread.getLooper(), this);
+                }
+                backHandler.sendEmptyMessageDelayed(DO_BG_TASK, DURATION);
+                uiHandler.sendEmptyMessageDelayed(DO_UI_TASK, DURATION);
+                v.setVisibility(View.GONE);
+                findViewById(R.id.btnStop).setVisibility(View.VISIBLE);
+            }break;
+            case R.id.btnStop : {
+                v.setVisibility(View.GONE);
+                findViewById(R.id.btnStart).setVisibility(View.VISIBLE);
+            }break;
+        }
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch(msg.what) {
+            case DO_UI_TASK:{
+                Log.d("BgTest", "uiMessage " + uiCount ++);
+                uiHandler.sendEmptyMessageDelayed(DO_UI_TASK, DURATION);
+            }break;
+            case DO_BG_TASK:{
+                Log.d("BgTest", "bgMessage " + bgCount++);
+                backHandler.sendEmptyMessageDelayed(DO_BG_TASK, DURATION);
+            }break;
+        }
+        return false;
+    }
+}
